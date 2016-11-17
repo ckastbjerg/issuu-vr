f79e18264ed255c9e01dbd4b9fcea7d6a1d694a3
Maciej Pekala <mp@issuu.com>
2016-11-18 00:34:32 +0100

    Add some clicking action

--- client/index.js
+++ client/index.js
@@ -22,7 +22,18 @@ function appendCover(url) {
 function appendTitle(id, width, index) {
     const length = width - 3;
 
+    var obj = document.createElement('a-entity');
     var obj = document.createElement('a-curvedimage');
+    var animation1 = document.createElement('a-animation');
+    var animation2 = document.createElement('a-animation');
+    animation1.setAttribute('attribute', `radius`);
+    animation1.setAttribute('to', `7`);
+    animation1.setAttribute('dur', `2000`);
+    animation1.setAttribute('begin', `do_scale`);
+    animation2.setAttribute('attribute', `theta-length`);
+    animation2.setAttribute('to', length * 2);
+    animation2.setAttribute('dur', `2000`);
+    animation2.setAttribute('begin', `do_scale`);
     obj.setAttribute('src', `assets/${id}.png`);
     obj.setAttribute('radius', '15');
     obj.setAttribute('theta-length', length);
@@ -31,15 +42,27 @@ function appendTitle(id, width, index) {
     obj.setAttribute('opacity', '.75');
     obj.setAttribute('crossorigin', 'anonymous'); // doesn't work :/
     // obj.setAttribute('cursor', 'fuse: true;');
-    // obj.addEventListener('click', () => {
-    //     console.log('test');
-    // });
+    obj.addEventListener('click', function () {
+        console.log(`assets/${id}.png`)
+        obj.setAttribute('opacity','1');
+        obj.emit('do_scale');
+    });
+    obj.appendChild(animation1);
+    obj.appendChild(animation2);
     scene.appendChild(obj);
 }
 
-appendBanner(categories[0].image);
+var scene = document.querySelector('a-scene');
+if (scene.hasLoaded) {
+    run();
+} else {
+    scene.addEventListener('loaded', run);
+}
+function run () {
+    appendBanner(categories[0].image);
 
-const width = 360 / categories.length;
-categories.forEach((category, index) => {
-    appendTitle(category.id, width, index);
-});
+    const width = 360 / categories.length;
+    categories.forEach((category, index) => {
+        appendTitle(category.id, width, index);
+    });
+}
